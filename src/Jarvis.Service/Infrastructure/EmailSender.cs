e67f5a91b7cb3d939717a436b89d366592819f97
TS <just.skorik@gmail.com>
2019-05-04 16:40:30 +0300

    Added core Jarvis service

---
+++ src/Jarvis.Service/Infrastructure/EmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jarvis.Service.Infrastructure
+{
+	class EmailSender
+	{
+	}
+}
---
+++ src/Jarvis.Service/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Jarvis.Service
+{
+	class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			await CreateWebHostBuilder(args).Build().RunAsync();
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+			.UseStartup<Startup>();
+	}
+}
