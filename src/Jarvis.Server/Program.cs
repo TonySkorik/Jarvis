663cde8ed1b1531e1858a886af72b74e12cf87e1
TS <just.skorik@gmail.com>
2020-09-20 23:36:52 +0300

    Add quratz hosted service packages. Work in progress

--- src/Jarvis.Server/Program.cs
+++ src/Jarvis.Server/Program.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Quartz;
+using Quartz.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
 
@@ -55,7 +57,62 @@ namespace Jarvis.Server
 						(cb) => AppBuilder.BuildContainer(cb, cntxt)
 					)
 				)
-				.ConfigureServices(s => s.AddHostedService<WaterCheckerService>())
+				.ConfigureServices(
+					s =>
+					{
+						s.AddHostedService<WaterCheckerService>();
+
+						s.AddQuartz(q =>
+						{
+							// handy when part of cluster or you want to otherwise identify multiple schedulers
+							q.SchedulerId = "Scheduler-Core";
+
+							// we take this from appsettings.json, just show it's possible
+							// q.SchedulerName = "Quartz ASP.NET Core Sample Scheduler";
+
+							// we could leave DI configuration intact and then jobs need to have public no-arg constructor
+							// the MS DI is expected to produce transient job instances 
+							q.UseMicrosoftDependencyInjectionJobFactory(options =>
+							{
+								// if we don't have the job in DI, allow fallback to configure via default constructor
+								options.AllowDefaultConstructor = true;
+							});
+
+							// or 
+							// q.UseMicrosoftDependencyInjectionScopedJobFactory();
+
+							// these are the defaults
+							q.UseSimpleTypeLoader();
+							q.UseInMemoryStore();
+							q.UseDefaultThreadPool(tp =>
+							{
+								tp.MaxConcurrency = 10;
+							});
+
+							// configure jobs with code
+							var jobKey = new JobKey("awesome job", "awesome group");
+							q.AddJob<ExampleJob>(j => j
+								.StoreDurably()
+								.WithIdentity(jobKey)
+								.WithDescription("my awesome job")
+							);
+
+							q.AddTrigger(t => t
+								.WithIdentity("Simple Trigger")
+								.ForJob(jobKey)
+								.StartNow()
+								.WithSimpleSchedule(x => x.WithInterval(TimeSpan.FromSeconds(10)).RepeatForever())
+								.WithDescription("my awesome simple trigger")
+							);
+
+						});
+
+						s.AddQuartzServer(options =>
+						{
+							// when shutting down we want jobs to complete gracefully
+							options.WaitForJobsToComplete = true;
+						});
+					})
 				.ConfigureWebHostDefaults(
 					webBuilder =>
 					{
