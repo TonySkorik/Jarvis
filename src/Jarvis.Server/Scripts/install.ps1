139cb7b254cdb251869a8f1ec53cc359b154dade
TS <just.skorik@gmail.com>
2020-10-18 22:16:12 +0300

    Update install script

--- src/Jarvis.Server/Scripts/install.ps1
+++ src/Jarvis.Server/Scripts/install.ps1
@@ -1,14 +1,14 @@
-$exePath = "./"
+$binariesPath = "../"
 $exeFilePath = "Jarvis.Server.exe"
 $userName = "justs"
 $serviceName = "Jarvis.Server"
 $description = "Jarvis Core Server Component"
 $displayName = "Jarvis Server"
 
-$acl = Get-Acl $exePath
+$acl = Get-Acl $binariesPath
 $aclRuleArgs = $userName, "Read,Write,ReadAndExecute", "ContainerInherit,ObjectInherit", "None", "Allow"
 $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($aclRuleArgs)
 $acl.SetAccessRule($accessRule)
-$acl | Set-Acl $exePath
+$acl | Set-Acl $binariesPath
 
 New-Service -Name $serviceName -BinaryPathName $exeFilePath -Credential $userName -Description $description -DisplayName $displayName -StartupType Automatic
