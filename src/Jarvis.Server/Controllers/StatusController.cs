0e197b61c5469a0c70f75c08fbbe2c43462a76a5
TS <just.skorik@gmail.com>
2021-01-31 20:35:21 +0300

    Add status controller; Move to net5; Add docker support

---
+++ src/Jarvis.Server/Controllers/StatusController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Jarvis.Server.Configuration;
+using Jarvis.Server.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Jarvis.Server.Controllers
+{
+	[Route("api/status")]
+	[ApiController]
+	public class StatusController : ControllerBase
+	{
+		private readonly AppSettings _settings;
+		private readonly ILogger<StatusController> _logger;
+
+		public StatusController(AppSettings settings, ILogger<StatusController> logger)
+		{
+			_settings = settings;
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public RStatus GetStatus()
+		{
+			return new(
+				Environment.OSVersion.VersionString,
+				Environment.ProcessorCount,
+				Environment.ProcessId,
+				Environment.CommandLine);
+		}
+	}
+}
---
+++ src/Jarvis.Server/Model/RStatus.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jarvis.Server.Model
+{
+	public record RStatus(string Os, int ProcessorCount, int ProcessId, string CommandLine);
+}
